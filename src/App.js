9ed6d9571693a7c51a2c7b739ef15a5f2f4993f7
LailaNouman <lailaibraheem@hotmail.com>
2022-05-14 22:23:48 +0300

    task17done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './components/home/Home';
-
+import Tour from './components/tour/Tour';
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-   <Home/>
+    <>
+   {/* <Home/> */}
+   <Routes>
+   <Route path="/" element={<Home />} />
+   <Route path="/tour" element={<Tour />} />
+   </Routes>
+    </>
   );
 }
 
